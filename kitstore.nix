8340ed569694c9f35e914ecfd0e170878ae8664b
starbased <s@starbased.net>
2026-03-31 19:15:23 -0700

    chore: add direnv-instant repository to kitstore

--- kitstore.nix
+++ kitstore.nix
@@ -1,5 +1,8 @@
 {
   repositories = {
+    "nix/direnv-instant" = {
+      url = "https://github.com/Mic92/direnv-instant";
+    };
     smfh = {
       url = "https://github.com/feel-co/smfh";
     };
