e5d1b4eab6e33dfe0d5bd80d4a72b14a55425df8
starbased <s@starbased.net>
2026-07-08 21:10:02 -0700

    refactor!: migrate kitstore.nix from repositories to use syntax
    Restructures configuration to use node-based mount declarations instead
    of URL-based repository definitions.
    
    BREAKING CHANGE: kitstore.nix now requires 'use' array with node/mount
      objects instead of 'repositories' map

--- kitstore.nix
+++ kitstore.nix
@@ -1,10 +1,6 @@
 {
-  repositories = {
-    "nix/direnv-instant" = {
-      url = "https://github.com/Mic92/direnv-instant";
-    };
-    smfh = {
-      url = "https://github.com/feel-co/smfh";
-    };
-  };
+  use = [
+    { node = "@repo:direnv-instant"; mount = "nix/direnv-instant"; }
+    { node = "@repo:smfh"; mount = "smfh"; }
+  ];
 }
